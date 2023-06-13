369eebee9da67f5c4ef2ca4e836a85053591d2f1
vicantlop <vicantlop@gmail.com>
2023-06-12 18:48:41 -0700

    created form and work weather app forecast

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import { useState } from 'react';
+
+
 
 function App() {
+  const [city, setCity] = useState("")
+  const [days, setDays] = useState("")
+
+  const setWeather = (event) => {
+    event.preventDefault();
+    switch (event.target.name) {
+      case 'city':
+        setCity(event.target.value);
+        break;
+      case 'days':
+        setDays(event.target.value);
+        break;
+      default:
+        break;
+    }
+    console.log(city, days)
+    console.log(typeof days)
+  }
+
+  const options = {
+    method: 'GET',
+    url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+    params: {
+      q: `${city}`,
+      days: `${days}`
+    },
+    headers: {
+      'X-RapidAPI-Key': '187b0ec4b7msh9909e74c3e40f95p17b4b6jsn67ee1ffae374',
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+  };
+
+  const getWeather = async (event) => {
+    console.log(city,days)
+    event.preventDefault()
+    console.log(options)
+    
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="city">City:</label><br/>
+        <input type="text" id="city" name="city" onChange={setWeather}></input><br/>
+        <label htmlFor="days">Days:</label><br/>
+        <input type="text" id="days" name="days" onChange={setWeather}></input><br/>
+        <input type="submit" value="Submit" onClick={getWeather}/>
+      </form>
     </div>
   );
 }
