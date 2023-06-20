bb7496e1ae5ab31f0ed6a97c71ffa5a323083a88
vicantlop <vicantlop@gmail.com>
2023-06-20 12:51:33 -0700

    created dropdown for search

--- src/components/Search.js
+++ src/components/Search.js
@@ -83,18 +83,18 @@ const Search = () => {
         <div>
             <form>
                 <label htmlFor="city">City:</label><br />
-                <input type="text" id="city" name="city" onChange={setWeather}></input><br />
+                <input type="text" id="city" className="dropdown-trigger" data-target='dropdown1' name="city" onChange={setWeather}></input><br />
                 <input type="submit" id="citySubmit" value="Submit" onClick={getWeather} />
-                <a class='dropdown-trigger btn' href='#/' data-target='dropdown1'>Drop Me!</a>
+                {/* <a className='dropdown-trigger btn' href='#/' data-target='dropdown1'>Drop Me!</a> */}
 
                 {/* <!-- Dropdown Structure --> */}
-                <ul id='dropdown1' class='dropdown-content'>
+                <ul id='dropdown1' className='dropdown-content'>
                     <li><a href="#!">one</a></li>
                     <li><a href="#!">two</a></li>
-                    <li class="divider" tabindex="-1"></li>
+                    <li className="divider" tabIndex="-1"></li>
                     <li><a href="#!">three</a></li>
-                    <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-                    <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+                    <li><a href="#!"><i className="material-icons">view_module</i>four</a></li>
+                    <li><a href="#!"><i className="material-icons">cloud</i>five</a></li>
                 </ul>
             </form>
             <CardsContainer>
